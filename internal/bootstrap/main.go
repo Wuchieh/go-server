903af094e5b809083664dba0c558ede638a91254
Wuchieh <q20001116@gmail.com>
2025-09-14 14:21:34 +0800

    refactor: lint fix

--- internal/bootstrap/main.go
+++ internal/bootstrap/main.go
@@ -1,12 +1,12 @@
 package bootstrap
 
-import (
-	"github.com/Wuchieh/go-server/internal/utils/logger"
-)
+import "github.com/Wuchieh/go-server/internal/utils/logger"
 
 func Run() {
 	initConfig()
 
 	loggerSetup()
-	defer logger.Sync()
+	defer func() {
+		_ = logger.Sync()
+	}()
 }
